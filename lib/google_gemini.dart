64789be6491aea245079491b32c8675a4ff48d8a
ged-flod <codewithuse@gmail.com>
2023-12-07 09:19:16 +0100

    Add: Unit test - create google gemini instance

--- lib/google_gemini.dart
+++ lib/google_gemini.dart
@@ -5,12 +5,12 @@ class GoogleGemini {
   
   String apiKey; // The API Key from Google
 
-  GoogleGemini(this.apiKey);
+  GoogleGemini({required this.apiKey});
 
   // Ask to google gemini api
   Future<String> ask(String query) {
     // TODO: implement ask
     return Future.value("Hello, I'm Google Gemini, $query");
   }
-  
+
 }
--- test/google_gemini_test.dart
+++ test/google_gemini_test.dart
@@ -3,10 +3,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:google_gemini/google_gemini.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  test('create google gemini instance', () {
+    final gemini = GoogleGemini(apiKey:"API_KEY");
+    expect(gemini.apiKey, "API_KEY");
   });
 }
