a0610474decba2a9e75d25f973312bb57b2c0cfe
ged-flod <codewithuse@gmail.com>
2023-12-16 21:16:49 +0100

    Add Text With image input

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,9 @@
-import 'dart:developer';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_gemini/google_gemini.dart';
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+
+const apiKey = "AIzaSyDybIK0Bw6WYe-ZFM235KWRL2xxYuX8ryQ";
 
 void main() {
   runApp(const MyApp());
@@ -32,6 +34,48 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      initialIndex: 0,
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Google Gemini"),
+          centerTitle: true,
+          bottom: const TabBar(
+            tabs: [
+              Tab(text: "Text Only"),
+              Tab(text: "Text with Image"),
+            ],
+          ),
+        ),
+
+        body: const TabBarView(
+          children: [
+            TextOnly(),
+            TextWithImage()
+          ],
+        )
+      )
+    );
+  }
+}
+
+
+
+
+// ------------------------------ Text with Image ------------------------------
+
+class TextOnly extends StatefulWidget {
+  const TextOnly({super.key,});
+
+  @override
+  State<TextOnly> createState() => _TextOnlyState();
+}
+
+class _TextOnlyState extends State<TextOnly> {
+
   bool loading = false;
   bool textOnly = false;
   List textChat = [];
@@ -43,7 +87,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   // Create Gemini Instance 
   final gemini = GoogleGemini(
-    apiKey: "AIzaSyAP7T_WduUuX0rRjmTLfYJPQnlJsvMkI1k",
+    apiKey: apiKey,
   );
 
   // Text only input 
@@ -70,8 +114,16 @@ class _MyHomePageState extends State<MyHomePage> {
       });
       scrollToTheEnd();
       
-    })
-    .catchError((e) => log(e));
+    }).onError((error, stackTrace) {
+      setState(() {
+        loading = false;
+        textChat.add({
+          "role": "Gemini",
+          "text": error.toString(),
+        });
+      });
+      scrollToTheEnd();
+    });
   }
 
   void scrollToTheEnd(){
@@ -81,12 +133,6 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      appBar: AppBar(
-        title: const Text("Google Gemini"),
-        centerTitle: true,
-      ),
-
       body: Column(
         children: [
           Expanded(
@@ -109,8 +155,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
           Container(
             alignment: Alignment.bottomRight,
-            margin: EdgeInsets.all(10.0),
-            padding: EdgeInsets.symmetric(horizontal: 15.0),
+            margin: const EdgeInsets.all(20),
+            padding: const EdgeInsets.symmetric(horizontal: 15.0),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(10.0),
               border: Border.all(color: Colors.grey),
@@ -133,7 +179,6 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                   ),
                   IconButton(
-
                     icon: loading 
                       ?  const CircularProgressIndicator()
                       : const Icon(Icons.send),
@@ -151,3 +196,176 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+
+
+
+
+
+
+// ------------------------------ Text with Image ------------------------------
+
+class TextWithImage extends StatefulWidget {
+  const TextWithImage({super.key,});
+
+  @override
+  State<TextWithImage> createState() => _TextWithImageState();
+}
+
+class _TextWithImageState extends State<TextWithImage> {
+
+  bool loading = false;
+  bool textOnly = false;
+  List textAndImageChat = [];
+  List textWithImageChat = [];
+  File? imageFile;
+
+  final ImagePicker picker = ImagePicker();
+
+  final TextEditingController _textController = TextEditingController();
+  final ScrollController _controller = ScrollController();
+
+
+  // Create Gemini Instance 
+  final gemini = GoogleGemini(
+    apiKey: apiKey,
+  );
+
+  // Text only input 
+  void fromTextAndImage({required String query, required File image}) {
+    setState(() {
+      loading = true;
+      textAndImageChat.add({
+        "role": "User",
+        "text": query,
+        "image": image,
+      });
+      _textController.clear();
+      imageFile = null;
+    });
+    scrollToTheEnd();
+   
+
+    gemini.generateFromTextAndImages(query: query, image: image)
+    .then((value){
+      setState(() {
+        loading = false;
+        textAndImageChat.add({
+          "role": "Gemini",
+          "text": value.text,
+          "image": ""
+        });
+      });
+      scrollToTheEnd();
+      
+    }).onError((error, stackTrace) {
+      setState(() {
+        loading = false;
+        textAndImageChat.add({
+          "role": "Gemini",
+          "text": error.toString(),
+          "image": ""
+        });
+      });
+      scrollToTheEnd();
+    });
+  }
+
+  void scrollToTheEnd(){
+    _controller.jumpTo(_controller.position.maxScrollExtent);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              controller: _controller,
+              itemCount: textAndImageChat.length,
+              padding: const EdgeInsets.only(bottom: 20),
+              itemBuilder: (context, index) {
+                return ListTile(
+                  isThreeLine: true,
+                  leading: CircleAvatar(
+                    child: Text(textAndImageChat[index]["role"].substring(0, 1)),
+                  ),
+                  title: Text(textAndImageChat[index]["role"]),
+                  subtitle: Text(textAndImageChat[index]["text"]),
+                  trailing: textAndImageChat[index]["image"] == "" 
+                    ? null
+                    : Image.file(textAndImageChat[index]["image"], width: 90,),
+                );
+              },
+            ),
+          ),
+
+          Container(
+            alignment: Alignment.bottomRight,
+            margin: const EdgeInsets.all(20),
+            padding: const EdgeInsets.symmetric(horizontal: 15.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+              border: Border.all(color: Colors.grey),
+            ),
+            child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: _textController,
+                      decoration: InputDecoration(
+                        hintText: "Write a message",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          borderSide: BorderSide.none
+                        ),
+                        fillColor: Colors.transparent,
+                      ),
+                      maxLines: null,
+                      keyboardType: TextInputType.multiline,
+                    ),
+                  ),
+                  IconButton(
+                    icon:const Icon(Icons.add_a_photo),
+                    onPressed: () async{
+                     final XFile? image = await picker.pickImage(source: ImageSource.gallery);
+                     setState(() {
+                      imageFile = image != null ? File(image.path) : null;
+                     });
+                    },
+                  ),
+                  IconButton(
+                    icon: loading 
+                      ?  const CircularProgressIndicator()
+                      : const Icon(Icons.send),
+                    onPressed: () {
+                      if(imageFile == null) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text("Please select an image"))
+                        );
+                        return;
+                      }
+                      fromTextAndImage(query: _textController.text, image: imageFile!);
+                    },
+                  ),
+                ],
+              ),
+              
+          ),
+        ],
+      ),
+      floatingActionButton: imageFile != null ? Container(
+        margin: const EdgeInsets.only(bottom: 80),
+        height: 150,
+        child: Image.file(imageFile ?? File("")),
+      ): null,
+      
+    );
+  }
+}
+
+
+
+
+
