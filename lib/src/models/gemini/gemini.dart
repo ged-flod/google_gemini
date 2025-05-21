549c32d97b815cf39f5b7f1ecbcf1795c93701e8
Jeremiah Oluwaseun Erinola <seunjeremiah@gmail.com>
2025-05-21 01:43:01 +0100

    Removed Hardcoded model ||Update gemini.dart

--- lib/src/models/gemini/gemini.dart
+++ lib/src/models/gemini/gemini.dart
@@ -28,7 +28,7 @@ class GoogleGemini {
         apiKey: apiKey,
         config: config,
         safetySettings: safetySettings,
-        model: "gemini-pro");
+        model: model);
 
     if (httpResponse.candidates.isNotEmpty) {
       for (var part in httpResponse.candidates[0].content!['parts']) {
@@ -51,7 +51,7 @@ class GoogleGemini {
         image: image,
         config: config,
         safetySettings: safetySettings,
-        model: "gemini-pro-vision");
+        model: model);
 
     if (httpResponse.candidates.isNotEmpty) {
       for (var part in httpResponse.candidates[0].content!['parts']) {
